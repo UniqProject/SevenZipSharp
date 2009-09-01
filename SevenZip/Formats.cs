bb8b2097023b921aad9028ba88bf8f9e20749fc7
SND\markhor_cp <SND\markhor_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2009-09-01 13:12:28 +0000

    Added LZMA2 method support. It works :)
    git-svn-id: https://sevenzipsharp.svn.codeplex.com/svn@27612 9b283d60-5439-405e-af05-b73fd8c4d996

--- SevenZip/Formats.cs
+++ SevenZip/Formats.cs
@@ -255,33 +255,37 @@ namespace SevenZip
     public enum CompressionMethod
     {
         /// <summary>
-        /// Zip or 7-zip|no compression method
+        /// Zip or 7-zip|no compression method.
         /// </summary>
         Copy,
         /// <summary>
-        /// Zip|Deflate method
+        /// Zip|Deflate method.
         /// </summary>
         Deflate,
         /// <summary>
-        /// Zip|Deflate64 method
+        /// Zip|Deflate64 method.
         /// </summary>
         Deflate64,
         /// <summary>
-        /// Zip or 7-zip|Bzip2 method
+        /// Zip or 7-zip|Bzip2 method.
         /// </summary>
         /// <remarks><a href="http://en.wikipedia.org/wiki/Cabinet_(file_format)">Wikipedia information</a></remarks>
         BZip2,
         /// <summary>
-        /// Zip or 7-zip|LZMA method based on Lempel-Ziv algorithm, it is default for 7-zip
+        /// Zip or 7-zip|LZMA method based on Lempel-Ziv algorithm, it is default for 7-zip.
         /// </summary>
         Lzma,
         /// <summary>
-        /// 7-zip|PPMd method based on Dmitry Shkarin's PPMdH source code, very efficient for compressing texts
+        /// 7-zip|LZMA version 2, LZMA with improved multithreading and usually slight archive size decrease.
+        /// </summary>
+        Lzma2,
+        /// <summary>
+        /// Zip or 7-zip|PPMd method based on Dmitry Shkarin's PPMdH source code, very efficient for compressing texts.
         /// </summary>
         /// <remarks><a href="http://en.wikipedia.org/wiki/Prediction_by_Partial_Matching">Wikipedia information</a></remarks>
         Ppmd,
         /// <summary>
-        /// No method change
+        /// No method change.
         /// </summary>
         Default
     }
@@ -371,6 +375,7 @@ namespace SevenZip
                 {CompressionMethod.Deflate, "Deflate"},
                 {CompressionMethod.Deflate64, "Deflate64"},
                 {CompressionMethod.Lzma, "LZMA"},
+                {CompressionMethod.Lzma2, "LZMA2"},
                 {CompressionMethod.Ppmd, "PPMd"},
                 {CompressionMethod.BZip2, "BZip2"}
             };
