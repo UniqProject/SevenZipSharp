07079605b4a93ed904cec85c59dd5bb24ef70a51
SND\markhor_cp <SND\markhor_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2010-04-15 06:11:06 +0000

    Fixed 0.61->0.62
    git-svn-id: https://sevenzipsharp.svn.codeplex.com/svn@47024 9b283d60-5439-405e-af05-b73fd8c4d996

--- SevenZip/Properties/AssemblyInfo.cs
+++ SevenZip/Properties/AssemblyInfo.cs
@@ -52,5 +52,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.61.*")]
-//[assembly: AssemblyFileVersion("0.6.0.*")]
+[assembly: AssemblyVersion("0.62.*")]
