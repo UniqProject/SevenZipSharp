120ae9b9b379c318fdbf210fc9b0643674d707bd
Juergen Tem <j.tem@gmx.de>
2014-02-15 20:22:38 +0100

    Create VS2013 solution + projects

--- SevenZip/Properties/AssemblyInfo.cs
+++ SevenZip/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.64.*")]
+[assembly: AssemblyVersion("0.65.*")]
